4b7f5f2853e6ed359286bf2c5b4a44baf6adee02
Rafael Gomes <rafabear@bearmachine-share.broadband>
2020-09-26 08:59:47 +0100

    add event listener to rest of btns

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const emojis = ["🕺", "⚽", "🎮"];
 
-let showEmojis = document.getElementById("showEmojis")
-let inputEmoji = document.getElementById("inputEmoji");
-let addStartBtn = document.getElementById("addEmojiStart");
+const showEmojis = document.getElementById("showEmojis")
+const inputEmoji = document.getElementById("inputEmoji");
+const addStartBtn = document.getElementById("addEmojiStart");
+const addEndBtn = document.getElementById("addEmojiEnd");
+const removeStartBtn = document.getElementById("removeEmojiStart");
+const removeEndBtn = document.getElementById("removeEmojiEnd");
 
 const updateDisplay = () => {
+    showEmojis.textContent = "";
     for (let i = 0; i < emojis.length; i++) {
         showEmojis.textContent += emojis[i];
     }
@@ -13,7 +17,6 @@ const updateDisplay = () => {
 const checkInput = () => {
     console.log(inputEmoji.value)
     if (inputEmoji.value) {
-        showEmojis.textContent = "";
         inputEmoji.value = "";
         updateDisplay();
     }
@@ -22,6 +25,21 @@ const checkInput = () => {
 updateDisplay()
 
 addStartBtn.addEventListener("click", () => {
+    emojis.unshift(inputEmoji.value);
+    checkInput();
+})
+
+addEndBtn.addEventListener("click", () => {
     emojis.push(inputEmoji.value);
     checkInput();
+})
+
+removeStartBtn.addEventListener("click", () => {
+    emojis.shift();
+    updateDisplay();
+})
+
+removeEndBtn.addEventListener("click", () => {
+    emojis.pop();
+    updateDisplay();
 })

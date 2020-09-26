00a9a9cf51b2902061d15fbb61fbee3f2ce51980
Rafael Gomes <rafabear@bearmachine-share.broadband>
2020-09-26 08:46:20 +0100

    add functions for updateDisplay and checkInput

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const emojis = ["🕺", "⚽", "🎮"];
 
 let showEmojis = document.getElementById("showEmojis")
+let inputEmoji = document.getElementById("inputEmoji");
+let addStartBtn = document.getElementById("addEmojiStart");
 
-for (let i = 0; i < emojis.length; i++) {
-    showEmojis.textContent += emojis[i];
+const updateDisplay = () => {
+    for (let i = 0; i < emojis.length; i++) {
+        showEmojis.textContent += emojis[i];
+    }
 }
+
+const checkInput = () => {
+    console.log(inputEmoji.value)
+    if (inputEmoji.value) {
+        showEmojis.textContent = "";
+        inputEmoji.value = "";
+        updateDisplay();
+    }
+}
+
+updateDisplay()
+
+addStartBtn.addEventListener("click", () => {
+    emojis.push(inputEmoji.value);
+    checkInput();
+})
